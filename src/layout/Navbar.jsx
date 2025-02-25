49e0788917ae7ea0f3d182d840c6a0c90387fe0c
Richard Marafioti <richmarafioti@gmail.com>
2025-02-25 10:54:54 -0600

    created accessibility menu with open and close functionality with the navbar item

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import { IoAccessibility } from "react-icons/io5";
 import styles from "../layout/navbar.module.css";
 
 export default function Navbar() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
   return (
-    <nav>
-      <p>This is my Navbar</p>
-      <IoAccessibility className={styles.accessIcon} />
-    </nav>
+    <>
+      <nav>
+        <p>This is my Navbar</p>
+        <IoAccessibility
+          className={styles.accessIcon}
+          onClick={() => setIsMenuVisible(true)}
+        />
+      </nav>
+
+      {isMenuVisible && (
+        <menu className={styles.accessMenu}>
+          <div>
+            <p>Accessibility Menu</p>
+            <ul>
+              <li>theme</li>
+              <li>cursor resizing</li>
+              <li>cursor highlighting</li>
+              <li>font size</li>
+              <li>font style</li>
+              <li>hide images</li>
+              <li>remove color</li>
+              <li>scroll to content</li>
+              <li>reset</li>
+            </ul>
+          </div>
+          <button onClick={() => setIsMenuVisible(false)}>Close</button>
+        </menu>
+      )}
+    </>
   );
 }
