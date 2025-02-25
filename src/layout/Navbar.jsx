bdabdb2d892bf59035b800f8f7ec3f790e11fcfc
Richard Marafioti <richmarafioti@gmail.com>
2025-02-25 10:28:00 -0600

    installed OpenDyslexic font styles and are showing up properly

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,11 @@
+import { IoAccessibility } from "react-icons/io5";
+import styles from "../layout/navbar.module.css";
+
 export default function Navbar() {
   return (
     <nav>
       <p>This is my Navbar</p>
+      <IoAccessibility className={styles.accessIcon} />
     </nav>
   );
 }
